f186ccdbc0da963569cf62d5c625fa879daf05f9
sergeichukd <sergeychukdmitry+git@gmail.com>
2020-06-15 02:34:19 +0300

    MetricValidator refactoring done

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,39 +1,50 @@
 class MetricValidator < ActiveModel::Validator
-    def validate(record)
-        current_year = Date.today.year
-        current_month = Date.today.month
-
-        last_record = Metric.where("user_id = '#{record.user_id}'").order(:created_at).last
-        if last_record.nil?
-            return
-        end
-
-        last_record_year = last_record.created_at.year
-        last_record_month = last_record.created_at.month
-
-        if (current_year <= last_record_year) && (current_month <= last_record_month)
-            record.errors[:date] << "Вы уже имеете актуальные показания на этот месяц"
-            return
-        end
-
-        if (record.hot < last_record.hot) || (record.cold < last_record.cold)
-            record.errors[:value] << "Введенные показания меньше последних: горячая: #{last_record.hot}, холодная: #{last_record.cold}"
-        end
-
-        if (record.hot < 0) || (record.cold < 0)
-            record.errors[:value] << "Вводимые значения должны быть больше нуля"
-        end
-        
+  def validate(record)
+    @last_record = Metric.where(user_id: record.user_id).first
+    if @last_record.nil?
+      return
     end
+
+    if actual_metric?
+      record.errors[:date] << 'Вы уже имеете актуальные показания на этот месяц'
+      return
+    end
+
+    if metric_decreased? record
+      record.errors[:value] << "Введенные показания меньше последних: горячая: #{@last_record.hot}, холодная: #{@last_record.cold}"
+    end
+
+    if metric_negative? record
+      record.errors[:value] << 'Вводимые значения должны быть больше нуля'
+    end
+  end
+
+  private
+
+  def actual_metric?
+    current_year = Date.today.year
+    current_month = Date.today.month
+
+    last_record_year = @last_record.created_at.year
+    last_record_month = @last_record.created_at.month
+
+    (current_year <= last_record_year) && (current_month <= last_record_month)
+  end
+
+  def metric_decreased?(record)
+    (record.hot < @last_record.hot) || (record.cold < @last_record.cold)
+  end
+
+  def metric_negative?(record)
+    record.hot.negative? || record.cold.negative?
+  end
 end
 
-
-
 class Metric < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
 
-    default_scope { order('created_at DESC') }
+  default_scope { order('created_at DESC') }
 
-    validates :cold, :hot, presence: true
-    validates_with MetricValidator, on: :create
+  validates :cold, :hot, presence: true
+  validates_with MetricValidator, on: :create
 end
