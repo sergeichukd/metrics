eef37e5f13f0bff360017649dc46ed2194a61f60
sergeichukd <sergeychukdmitry+git@gmail.com>
2020-06-15 02:06:13 +0300

    UsersController refactoring done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  def profile
-  end
-
   def new_password
     @user = current_user
   end
@@ -9,22 +6,19 @@ class UsersController < ApplicationController
   def update_password
     @user = current_user
     if @user.update(user_params)
-      # Sign in the user by passing validation in case their password changed
       @user.update(has_default_password: false)
+      # Sign in the user by passing validation in case their password changed
       bypass_sign_in(@user)
+
       redirect_to root_path
     else
       render :new_password
     end
   end
 
-
   private
 
-    def user_params
-      # NOTE: Using `strong_parameters` gem
-      params.require(:user).permit(:password, :password_confirmation)
-    end
-
-
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
